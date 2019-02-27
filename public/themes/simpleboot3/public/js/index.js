cb559d00e417ea35abdacd1688527e3f16837de8
smilesjuan <38411688+smilesjuan@users.noreply.github.com>
2019-02-27 09:49:29 +0800

    好

--- public/themes/simpleboot3/public/js/index.js
+++ public/themes/simpleboot3/public/js/index.js
@@ -94,6 +94,7 @@ $(".personBtn>.callOff").click(function(){
 	$(this).parents(".invocatedisplayform").prev(".invocateform-total").show();
 });
 
+//  订单页面选择优惠券
 
 
 
@@ -122,94 +123,262 @@ $(".personBtn>.callOff").click(function(){
 
 
 
-var t = $("#text_box");
-$("#add").click(function(){
-t.val(parseInt(t.val())+1)
-setTotal();
-})
-$("#min").click(function(){
-t.val(parseInt(t.val())-1)
-setTotal();
-})
-function setTotal(){
-var tt = $("#text_box").val();
-var pbinfoid=$("#pbinfoid").val();
-if(tt<=0){
-alert('输入的值错误！');
-t.val(parseInt(t.val())+1)
-}else{
-window.location.href = "shopping!updateMyCart.action?pbinfoid="+pbinfoid+"&number="+tt;
-}
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
 	
 	
 	
 	
 });
 
+
+
+
+
+
+// 商品详情页面的价格变化
+function setTotal(){
+	var allPrice = 0; // 总价格
+	$(".stepItem").each(function(){
+		var singleNum = parseInt($(".totalNum").val());   //单个商品的购买数量
+		var stepNum = parseInt($(this).find("span").text());
+		if(singleNum >= stepNum){
+			var singlePrice = parseFloat($(this).next("td").find("span").text());
+			console.log(singlePrice);
+			$(".detail_pri").find("i").text(singlePrice);
+			allPrice = singleNum*singlePrice;
+		}
+		$(".allPrice").html(allPrice.toFixed(2));
+	});
+	
+}
+setTotal();
+
+
+
+//手动修改文本框商品数量与库存的限制
+function amount_input(tag,sellprice,stock){
+	var amount=parseInt($(tag).val());
+	if(isNaN(amount)){
+		alert('最少购买量为1');
+		$(tag).val(1);
+	}else{
+		if(amount>stock){
+			alert('购买数量不能大于库存');
+			$(tag).val(stock);
+		}else if(amount<1){
+			alert('最少购买量为1');
+			$(tag).val(1);
+		}
+	}
+	var val=parseFloat(sellprice)*parseInt($(tag).val());
+	setTotal();
+}
+
 // 购买数量加
 function plus(tag,sellprice,stock){
 	var _this=$(tag);
 	var input=_this.prev('input');
-	if(_this.prev('input[disabled]').length>0){
-		return;
-	}
 	var amount=parseInt(input.val());
 	amount++;
 	if(amount>stock){
-		return layer.msg('购买数量不能大于库存');
+		alert('购买数量不能大于库存');
 	}else{
 		input.val(amount);
-//		setTotal();
+		setTotal();
 	}
 }
 // 购买数量减
  function minus(tag,sellprice,stock){
 	var _this=$(tag);
 	var input=_this.next('input');
-	if(_this.next('input[disabled]').length>0){
-		return;
-	}
 	var amount=parseInt(input.val());
 		amount--;
 	if(amount<=0){
-		return layer.msg('购买数量不能小于1');
+		return alert('购买数量不能小于1');
 	}else{
 		input.val(amount);
-//	setTotal();
+		setTotal();
+	}
+}
+ 
+ //  订单页面商品总价格变化
+ function setTotalOrder(){
+ 	var totalAmount = 0;// 总价格
+	$(".stepsNumber").each(function(){
+		var parInt = $(this).parents("td");
+		var orderNum = parseInt(parInt.siblings(".addUp").find(".orderNum").val());   //单个商品的购买数量
+		var stepsNum = parseInt($(this).find("span").text()); // 阶梯数量
+		if(orderNum >= stepsNum){
+			var stepsPrice = parseFloat($(this).next(".steps_price").find("span").text()); // 阶梯单价
+			console.log(stepsPrice);
+			totalAmount = stepsPrice*orderNum;
+			console.log(totalAmount);
+		}
+		$(this).parents(".padStep").next().next(".addUp").find(".totalPrice span").html(totalAmount.toFixed(2));
+	});
+ 	
+ }
+ setTotalOrder();
+//订单页面手动修改文本框商品数量与库存的限制
+function amount_input_order(tag,sellprice,stock){
+	var amount=parseInt($(tag).val());
+	if(isNaN(amount)){
+		alert('最少购买量为1');
+		$(tag).val(1);
+	}else{
+		if(amount>stock){
+			alert('购买数量不能大于库存');
+			$(tag).val(stock);
+		}else if(amount<1){
+			alert('最少购买量为1');
+			$(tag).val(1);
+		}
+	}
+	var val=parseFloat(sellprice)*parseInt($(tag).val());
+	setTotalOrder();
+}
+ 
+ // 数量增加
+function plusOrder(tag,sellprice,stock){
+	var _this=$(tag);
+	var input=_this.prev('input');
+	var amount=parseInt(input.val());
+	amount++;
+	if(amount>stock){
+		alert('购买数量不能大于库存');
+	}else{
+		input.val(amount);
+		setTotalOrder();
+	}
+}
+// 数量减少
+function minusOrder(tag,sellprice,stock){
+	var _this=$(tag);
+	var input=_this.next('input');
+	var amount=parseInt(input.val());
+		amount--;
+	if(amount<=0){
+		return alert('购买数量不能小于1');
+	}else{
+		input.val(amount);
+		setTotalOrder();
 	}
 }
 
 
 
 
+// 购物车页面订单结算
+function setTotalScart(){
+	$(".shopTable tr.shopdetail").each(function(){
+		var scartPrice =$(this).children(".scartPrice").find("span").text();
+		 	scartNum = $(this).find(".onum").val();
+	
+		var smallAmount = scartPrice*scartNum;
+		$(this).find(".subtotal span").html(smallAmount.toFixed(2));
+	});
+	
+}	
+setTotalScart();
+
+$(".shop-check").click(function(){
+	var check=$(".shop-check").length; 
+	console.log(check);
+	var checked=$(".shop-check:checked").length; 
+	console.log(checked);
+	if(check==checked){ 
+		$("#checkAll").prop("checked",true);
+	}else{ 
+		$("#checkAll").prop("checked",false);
+	} 	
+	checkNum();	
+	allPrice();
+});		
+
+// 购物车全选
+$("#checkAll").click(function(){
+	if(this.checked){
+		$(".shop-check").prop("checked",true);
+	}else{
+		$(".shop-check").prop("checked",false);
+	}
+	checkNum();	
+	allPrice();
+	
+});
 
 
-
+//购物车订单页面手动修改文本框商品数量与库存的限制
+function amount_input_Scart(tag,sellprice,stock){
+	var amount=parseInt($(tag).val());
+	if(isNaN(amount)){
+		alert('最少购买量为1');
+		$(tag).val(1);
+	}else{
+		if(amount>stock){
+			alert('购买数量不能大于库存');
+			$(tag).val(stock);
+		}else if(amount<1){
+			alert('最少购买量为1');
+			$(tag).val(1);
+		}
+	}
+	setTotalScart();
+	checkNum();
+	allPrice();
+}
+// 数量增加
+function plusScart(tag,sellprice,stock){
+	var _this=$(tag);
+	var input=_this.prev('input');
+	var amount=parseInt(input.val());
+	amount++;
+	if(amount>stock){
+		alert('购买数量不能大于库存');
+	}else{
+		input.val(amount);
+		setTotalScart();
+		checkNum();
+		allPrice();
+	}
+}
+// 数量减少
+function minusScart(tag,sellprice,stock){
+	var _this=$(tag);
+	var input=_this.next('input');
+	var amount=parseInt(input.val());
+		amount--;
+	if(amount<=0){
+		return alert('购买数量不能小于1');
+	}else{
+		input.val(amount);
+		setTotalScart();
+		checkNum();
+		allPrice();
+	}
+}
+// 购买总数量
+function checkNum(){
+	var num = 0;
+	$(".shop-check").each(function(){
+		if(this.checked==true){ 
+			var snum = parseFloat($(this).parents("tr").find(".onum").val());
+			num+=Number(snum.toFixed(2)); 
+		} 
+	});
+	$(".allNum").html(num);
+}
+function allPrice(){ 
+	var sum=0;  // 总和
+	$(".shop-check").each(function(){ 
+		if(this.checked==true){
+			var ssum = parseFloat($(this).parents("tr").find(".subtotal span").text());
+			sum+=Number(ssum); 
+		} 
+	}); 
+	$(".allPrice1").html(sum.toFixed(2)); 
+}
 
 
 
